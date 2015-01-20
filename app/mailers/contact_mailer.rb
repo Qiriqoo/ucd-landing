2a2ef5efe22a61a8e2bb3deb8f5506c5c4522d75
sdiallo <sdiallo888@gmail.com>
2015-01-20 15:49:07 +0100

    send email message to admin

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,19 +1,20 @@
 class ContactMailer < ActionMailer::Base
-  default from: "contact@ultimate-cycle-distribution.com"
-  @@admins = ['fp.fabricepicard@gmail.com']
+  default from: 'Ultimate Cycle Distribution <contact@ultimate-cycle-distribution.com>'
+  @@admins = ['picard.fabrice.fp@gmail.com']
 
   def confirm_mail contact
     @contact = contact
     mail(
       to: contact.email,
-      subject: "votre email a éte enregistré"
+      subject: "votre email a été enregistré"
     )
   end
   def mail_to_admin contact
     admins = @@admins
+    @contact = contact
     mail(
       to: admins.slice!(0),
-      subject: 'un nouvel utilisateur a été enregistré'
+      subject: 'un nouvel utilisateur s\'est inscrit sur le site'
     )
     
   end
