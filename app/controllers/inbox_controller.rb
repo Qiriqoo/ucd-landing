c3b6450a622b6d125a4dfd80a766acb3a74c7f4f
sdiallo <sdiallo888@gmail.com>
2015-01-29 13:31:57 +0100

    simple indent commit

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -2,6 +2,6 @@ class InboxController < ApplicationController
   include Mandrill::Rails::WebHookProcessor
 
   def handle_inbound(event_payload)
-		Recipient.save_inbound_email(event_payload)
+    Recipient.save_inbound_email(event_payload)
   end
 end
