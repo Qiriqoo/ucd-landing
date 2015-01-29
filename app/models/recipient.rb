a8ef9dc8fbf6f341bd35fc9116d61aba685c507e
sdiallo <sdiallo888@gmail.com>
2015-01-29 14:15:57 +0100

    indent well

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,12 +1,12 @@
 class Recipient < ActiveRecord::Base
-	after_create :transfert_to_admin
+  after_create :transfert_to_admin
 
-	def self.save_inbond_email(event_playload)
-		playload = event_playload['msg']
-		Recipient.create!(from_email: playload['from_email'], subject: playload['subject'], msg: playload['html'])		
-	end
+  def self.save_inbond_email!(event_playload)
+    playload = event_playload['msg']
+  Recipient.create!(from_email: playload['from_email'], subject: playload['subject'], msg: playload['html'])
+  end
 
-	def transfert_to_admin recipient
-		ContactMailer.ucd_contact(self).deliver
-	end
+  def transfert_to_admin recipient
+    ContactMailer.ucd_contact(self).deliver
+  end
 end
